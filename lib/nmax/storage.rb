b9e17cec133c40c84edcb6f3b7b8726c12686d5e
Aleksandr Noskov <nemestny@politeh.ru>
2019-03-10 13:50:15 +0500

    Create add, each methods on class Storage

--- lib/nmax/storage.rb
+++ lib/nmax/storage.rb
@@ -1,9 +1,34 @@
 module Nmax
+
   class Storage
-    def initialize(args)
-      
+    def initialize(size:)
+      @sorted_storage = SortedSet.new
+      @size = size
     end
-    
-    
+
+    def add(number)
+      return if @sorted_storage.include?(number)
+      if @sorted_storage.size < @size
+        @sorted_storage.add(number)
+      else
+        custom_add(number)
+      end
+    end
+
+    def each(&block)
+      @sorted_storage.each(&block)
+    end
+
+    private
+
+    def custom_add(number)
+      min_number = @sorted_storage.min
+
+      if min_number < number
+        @sorted_storage.delete(min_number)
+        @sorted_storage.add(number)
+      end
+    end
+   
   end
 end
